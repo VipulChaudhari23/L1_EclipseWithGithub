f6f5ea41518b0ffa5945deaf272b18bf6b69f0ad
Vipul Chaudhari <vipulchaudhary2005@gmail.com>
2023-09-29 12:55:48 +0530

    New print statement

--- JAVA_with_GitHub/src/com/training/org/HelloGitHub.java
+++ JAVA_with_GitHub/src/com/training/org/HelloGitHub.java
@@ -3,7 +3,8 @@ package com.training.org;
 public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("Vipul");
-		System.out.println();
+		System.out.println("Hii Java");
+		System.out.println("NOT TO VS CODE");
 
 	}
 }
